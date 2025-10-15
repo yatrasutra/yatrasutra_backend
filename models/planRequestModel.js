8b96c4a832c00a99d2ff3f8c49e8100a82ad6e27
Risheb.s <rishebs123456@gmail.com>
2025-10-15 17:32:14 +0530

    changed price field

--- models/planRequestModel.js
+++ models/planRequestModel.js
@@ -1,7 +1,7 @@
 export const planRequestFields = {
     name: "string",         
     destination: "string",  
-    price: "number",       
+    price: "string",       
     phone: "string",       
   };
   
